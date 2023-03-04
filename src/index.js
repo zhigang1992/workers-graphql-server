1d3b8a2a9b6b2e40ea68831db7892a20db2467c1
Zhigang Fang <zhigang1992@gmail.com>
2023-03-04 12:32:23 +0800

    feat: use / as playground

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ const setCors = require('./utils/setCors')
 
 const graphQLOptions = {
   // Set the path for the GraphQL server
-  baseEndpoint: '/',
+  baseEndpoint: '/graphql',
 
   // Set the path for the GraphQL playground
   // This option can be removed to disable the playground route
-  playgroundEndpoint: '/___graphql',
+  playgroundEndpoint: '/',
 
   // When a request's path isn't matched, forward it to the origin
   forwardUnmatchedRequestsToOrigin: false,
